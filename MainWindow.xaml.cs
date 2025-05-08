efa56990443cbb4b1fe60d934a2ad4aaa2952792
Keano <keano.ruelle@student.vives.be>
2025-05-08 16:23:47 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_OOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> naam = new List<string>();
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnToonlijst_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
